0a762ce5a4cc9fc6a2c8181ac48b8c026ee40fbb
jinyong <zhaqh146@naver.com>
2023-07-17 17:39:39 +0900

    게시글 목록 진행 중

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { toggleState } from "../atoms";
+
+function Toggle() {
+
+  const [toggle, setToggle] = useRecoilState(toggleState);
+
+  function toggleButton(before) {
+    if (toggle === "post" && before === "place") {
+      setToggle("place");
+    } else if (toggle === "place" && before === "post") {
+      setToggle("post");
+    }
+  }
+
+  return(
+    <div className="h-[50px] flex justify-between">
+      <button 
+        onClick={() => toggleButton("post")} 
+        className={`
+          w-[160px]
+          rounded-t-lg
+          border-solid
+          border-t-[1.5px]
+          border-l-[1.5px]
+          border-r-[1.5px]
+          border-[#8449F5]
+          ${toggle === "post" ? "bg-[#8449F5]" : ""}
+          ${toggle === "post" ? "text-white" : "text-black"}
+          font-bold`}
+      >
+        Post
+      </button>
+      <button 
+        onClick={() => toggleButton("place")} 
+        className={`
+          w-[160px]
+          rounded-t-lg
+          border-solid
+          border-t-[1.5px]
+          border-l-[1.5px]
+          border-r-[1.5px]
+          border-[#8449F5]
+          ${toggle === "place" ? "bg-[#8449F5]" : ""}
+          ${toggle === "place" ? "text-white" : "text-black"}
+          font-bold`}
+      >
+        Place
+      </button>
+    </div>
+  )
+}
+
+export default Toggle;
